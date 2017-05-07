7538149a4e91ef744a4faddeaad04ccc67f7ff65
Vadim Zaliva <lord@crocodile.org>
2017-05-07 09:14:40 -0700

    Hello unit test example

--- exercises/hello-world/example.v
+++ exercises/hello-world/example.v
@@ -1,3 +1,10 @@
 Require Import Coq.Strings.String.
 
 Definition hello:string := "Hello, World!".
+
+(* Unit test *)
+Lemma HelloTest:
+  hello = "Hello, World!"%string .
+Proof.
+  reflexivity.
+Qed.
